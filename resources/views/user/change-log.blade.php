3318bbf7dd8451554f5d418f6df241f753a942f6
Chris Dominic P. Chan <chanchrisdominic@gmail.com>
2024-06-23 10:07:43 +0800

    added consistency

--- resources/views/user/change-log.blade.php
+++ resources/views/user/change-log.blade.php
@@ -38,7 +38,7 @@ Product Changelog
         @else
         <div class="list-group-item mb-2">
             <div class="row">
-                <p class="col-auto m-0">{{ $change->fk_product }}</p>
+                <p class="col-auto m-0">ID-{{ $change->fk_product }}</p>
                 <p class="col-auto {{ $change->isAdd() ? 'text-success' : 'text-danger' }} m-0 fw-bold">{{ ucwords($change->kind()) }}</p>
                 <p class="col m-0">{{ $change->product()->product_name }}</p>
                 <p class="col m-0 {{ $change->amount > 0 ? 'text-success' : 'text-danger' }}" style="font-size: 24px; line-height: 24px;">{{ $change->amount > 0 ? '+' : '' }}{{ $change->amount }} ({{ $change->amount > 0 ? 'Added' : 'Subtracted'}})</p>
